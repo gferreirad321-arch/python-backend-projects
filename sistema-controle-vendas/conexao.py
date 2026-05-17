f7d3023b08b0c628e1a6d125fd95061f061c65ea
Gabriel Ferreira <gferreirad321@gmail.com>
2026-05-17 12:58:44 -0300

    organiza projeto em estrutura profissional

---
+++ sistema-controle-vendas/conexao.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def conectar():
+    try:
+        conexao = mysql.connector.connect(
+            host='localhost',
+            user='seu_usuario',
+            password='sua_senha',
+            database='nome_do_banco'
+        )
+
+        if conexao.is_connected():
+            print("Conectado ao MySQL com sucesso!")
+            return conexao
+
+    except Error as e:
+        print(f"Erro ao conectar: {e}")
+        return None
+
+
+def fechar_conexao(conexao):
+    if conexao and conexao.is_connected():
+        conexao.close()
+        print("Conexão encerrada.")
